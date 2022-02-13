ff3201f7eb82ae6c470ad96c62865e34f35fc727
meghana19981 <78581407+meghana19981@users.noreply.github.com>
2022-02-13 14:23:21 -0500

    Update Escape.java

--- adventureGame/Escape.java
+++ adventureGame/Escape.java
@@ -18,8 +18,8 @@ public class Escape {
 
     public void storyLine() {
             //This is the intro to the game, the first part is story telling. Giving the setting of the game.
-            System.out.println("You are driving down an abondoned road late at night. You just got out of a late meeting and the gps has guided you down a new road.\n");
-            System.out.println("Trusting the GPS you begind to take turns and enter into roads you have never seen before. The roads are empty except for you,\n");
+            System.out.println("You are driving down an abandoned road late at night. You just got out of a late meeting and the gps has guided you down a new road.\n");
+            System.out.println("Trusting the GPS you begin to take turns and enter into roads you have never seen before. The roads are empty except for you,\n");
             
             //so there isnt too much dialogue on the screen, user has to press 0 to continue or press any number to display the full dialogue. 
             System.out.println("(Press '0' to continue dialogue or press any number to skip.\n)");
@@ -37,9 +37,9 @@ public class Escape {
 
                 if (next == 0){
 
-                    System.out.println("glowing under the moonlight. It looks intimidating and you try not to look at it for too long.Suddently you hear a weird noise\n");
+                    System.out.println("glowing under the moonlight. It looks intimidating and you try not to look at it for too long. Suddenly you hear a weird noise\n");
                     System.out.println("coming from your car and get worried. You have never heard it before until today. You try to ignore the noise until your car starts\n");
-                    System.out.println("jerking in an unusual manner. Suddently, the car begins to slow down and finally completely halts in the middle of the road. You try\n");
+                    System.out.println("jerking in an unusual manner. The car begins to slow down and finally completely halts in the middle of the road. You try\n");
 
 
                     System.out.println("(Press '0')");
@@ -47,7 +47,7 @@ public class Escape {
                     if (next == 0){
 
                         System.out.println("to turn it on multiple times but it doesnt work. You reach for your phone and try to call AAA hoping they will be able to help you.\n");
-                        System.out.println(">>With worry you dial the number and wait. The automated vocie begins to speak asking for what you need.\n");
+                        System.out.println(">>With worry you dial the number and wait. The automated voice begins to speak asking for what you need.\n");
 
 
 
@@ -62,9 +62,9 @@ public class Escape {
                 // if the user presses any other number the dialogue displays completely.
                 System.out.println(">>Trusting the GPS you begind to take turns and enter into roads you have never seen before. The roads are empty except for you,\n");
                 System.out.println("only a few streetlights illuminating the road. As you listen to the GPS you turn down a road and drive, you see a large house\n");
-                System.out.println("glowing under the moonlight. It looks intimidating and you try not to look at it for too long.Suddently you hear a weird noise\n");
+                System.out.println("glowing under the moonlight. It looks intimidating and you try not to look at it for too long. Suddenly you hear a weird noise\n");
                 System.out.println("coming from your car and get worried. You have never heard it before until today. You try to ignore the noise until your car starts\n");
-                System.out.println("jerking in an unusual manner. Suddently, the car begins to slow down and finally completely halts in the middle of the road. You try\n");
+                System.out.println("jerking in an unusual manner. The car begins to slow down and finally completely halts in the middle of the road. You try\n");
                 System.out.println("to turn it on multiple times but it doesnt work. You reach for your phone and try to call AAA hoping they will be able to help you.\n");
                 System.out.println(">>With worry you dial the number and wait. The automated voice begins to speak asking for what you need.\n");
 
@@ -80,7 +80,7 @@ public class Escape {
                 if(help == 1 || help == 2){
 
                     System.out.println("You press for help. The voice tells you that it will take more than 10 hours to get help.\n");
-                    System.out.println("Before you can refute the call ends. You look at your phone and notice your service is completely gone.\n");
+                    System.out.println("Before you can refute, the call ends. You look at your phone and notice your service is completely gone.\n");
                     System.out.println("Without a working phone or a working car you begin to get scared.\n");
                     System.out.println("You remember the house you just passed by. Without any other choice you get out of your car and head towards the house.\n");
 
@@ -116,7 +116,7 @@ public class Escape {
             knock = input.nextInt();
 
             if (knock == 1){
-                System.out.println("You knock on the door. With the force of the knock, the door opens.\n");
+                System.out.println("You knock on the door. The force of the knock, the door opens.\n");
                 System.out.println("It was unlocked the entire time.\n");
                 System.out.println("You enter into the house. Looking around, you notice that its old.\n");
                 System.out.println("and there are spider webs everywhere. The house looks abandoned.\n");
@@ -143,7 +143,7 @@ public class Escape {
                             System.out.println("You try to use more force, through a crack on the wall to the cieling,\n");
                             System.out.println("you hear the chandlier shake. You look up and notice its becoming loose.\n");
                             System.out.println("You leave the door and start running down the hall. The chandlier crashes but\n");
-                            System.out.println("you get out of the way before it falls. Theres debri everywhere.\n");
+                            System.out.println("you get out of the way before it falls. There's debri everywhere.\n");
 
                             System.out.print("(Press '0' to go into hallway)\n");
                             int next5 = input.nextInt();
@@ -162,8 +162,8 @@ public class Escape {
     }
 
     public boolean hallway() {
-        System.out.println(" Now you are in the hallway, there are three doors");
-        System.out.println("please enter a number from 1-3, to choose a door ");
+        System.out.println(" Now you are in the hallway, there are three doors\n");
+        System.out.println("please enter a number from 1-3, to choose a door\n");
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
 
@@ -201,12 +201,12 @@ public class Escape {
     public boolean theDoor1() {
 
         System.out.println("...");
-        System.out.println("In front of you are three items: ");
-        System.out.println("\n|A bat|   |A flashlight|   |A ladder|");
-        System.out.println("\nChoose wisely...");
-        System.out.println("\nType: \"bat\" to pick up the bat");
-        System.out.println("\nType: \"flashlight\" to pick up the flashlight");
-        System.out.println("\nType: \"ladder\" to pick up the ladder");
+        System.out.println("In front of you are three items: \n");
+        System.out.println("\n|A bat|   |A flashlight|   |A ladder|\n");
+        System.out.println("\nChoose wisely...\n");
+        System.out.println("\nType: \"bat\" to pick up the bat\n");
+        System.out.println("\nType: \"flashlight\" to pick up the flashlight\n");
+        System.out.println("\nType: \"ladder\" to pick up the ladder\n");
         String userItem = input.next();
 
 
@@ -216,17 +216,17 @@ public class Escape {
         boolean result = false;
         if (userItem.equals("bat") || userItem.equals ("Bat") || userItem.equals("BAT")){
             System.out.println("\nYou have picked up the " + userItem);
-            System.out.println("\nUnfortunately, you have tried hitting the windows down with \nyour bat but they won't break.");
+            System.out.println("\nUnfortunately, you have tried hitting the windows down with \nyour bat but they won't break.\n");
             result = false;
         }
         else if (userItem.equals("flashlight") || userItem.equals("Flashlight") || userItem.equals("FLASHLIGHT")){
             System.out.println("\nYou have picked up the " + userItem);
-            System.out.println("\nUnfortunately, you have tried finding a way out with \nyour flashlight but you're unsuccessful.");
+            System.out.println("\nUnfortunately, you have tried finding a way out with \nyour flashlight but you're unsuccessful.\n");
             result = false;
         }
         else if (userItem.equals("ladder") || userItem.equals("Ladder") || userItem.equals("LADDER")){
             System.out.println("\nGreat! You have picked up the " + userItem);
-            System.out.println("\nYou were able to climb up to a window with \nyour ladder and jump out!");
+            System.out.println("\nYou were able to climb up to a window with \nyour ladder and jump out!\n");
             result = true;
         }
         else {
@@ -247,9 +247,9 @@ public class Escape {
         int number4 = (int) (Math.random() * 5) + 5;
         int total = number1 + number2;
         int product = number3 * number4;
-        System.out.println(" Now you have the key to door2. This  door leads you to a basement" +
-                "\n where there is a locked door with a password. in order to get out of the house you need" +
-                "\n to enter the correct password ");
+        System.out.println(" Now you have the key to door2. This  door leads you to a basement\n" +
+        System.out.println("where there is a locked door with a password. in order to get out of the house you need\n");
+        System.out.println("\n to enter the correct password: \n");
         System.out.println("password hint: " + number1 + "+" + number2
                 + " space " + number3 + "*" + number4);
         Scanner input = new Scanner(System.in);
@@ -259,8 +259,8 @@ public class Escape {
             return true;
         }
         else {
-            System.out.println("wrong password" +
-                    "\n now you are back to the hallway");
+            System.out.println("wrong password\n");
+            System.out.println("\n now you are back to the hallway\n");
             return false;
         }
     }
@@ -268,43 +268,51 @@ public class Escape {
 
     // Meghana's door
     public boolean theDoor3() {
-        System.out.println("You entered into a door 3 with your key \n The room is dark, you cant see where you are.");
-        System.out.println("You feel around the wall next to you and find a light switch.");
+        System.out.println("You entered into a door 3 with your key \n The room is dark, you cant see where you are.\n");
+        System.out.println("You feel around the wall next to you and find a light switch.\n");
         boolean result = false;
         int light = 0;
         while (light != 1) {
-            System.out.println("Press '1' to turn the light on!");
+            System.out.println("Press '1' to turn the light on!\n");
             light = input.nextInt();
 
             if (light == 1) {
-                System.out.println("You continue to look around the garage. There are several items that you can look through!");
-                System.out.println("press '1' to look in shelf");
-                System.out.println("press '2' to look in box");
-                System.out.println("press '3' to look in trashcan");
-                System.out.println("press '4' to look in bag");
+                System.out.println("You continue to look around the garage. There are several items that you can look through!\n");
+                System.out.println("press '1' to look in shelf\n");
+                System.out.println("press '2' to look in box\n");
+                System.out.println("press '3' to look in trashcan\n");
+                System.out.println("press '4' to look in bag\n");
 
-                System.out.println("What do you pick?: ");
+                System.out.println("What do you pick?: \n");
                 int pick = input.nextInt();
 
                 if (pick == 1) {
 
-                    System.out.println("You picked the shelf. You rumage through shelf and didnt find anything...");
+                    System.out.println("You picked the shelf. You rumage through shelf and didnt find anything...\n");
                     result = false;
                 }
                 else if (pick == 2) {
 
-                    System.out.println("You picked the box.\n There are many items in the box.\n You look inside and find an object...\n It's a garage door opener!\n You press the button and it opens!");
+                    System.out.println("You picked the box. There are many items in the box.\n"); 
+                    System.out.println("You look inside and find an object that might be useful...\n");
+                    System.out.println("It's a garage door opener! You press the button and it opens!\n");                    
                     result = true;
                 }
                 else if (pick == 3) {
 
-                    System.out.println("You picked the trashcan...\n Something smells gross.\n You reach inside and a mouse jumps out!\n Theres nothing in here...");
+                    System.out.println("You picked the trashcan...\n");
+                    System.out.println("Something smells gross.\n");
+                    System.out.println("You reach inside and a mouse jumps out!\n");
+                    System.out.println("Theres nothing in here...\n");                    
                     result = false;
                 }
                 else if (pick == 4) {
 
-                    System.out.println("You picked the bag. You reach inside.\n There are random items inside. Looks like it was used for school.\n There are pencils and notebooks. Other then that...\n Theres nothing in here....");
-                    result = false;
+                   System.out.println("You picked the bag. You reach inside.\n");
+                   System.out.println("There are random items inside. Looks like it was used for school.\n");
+                   System.out.println("There are pencils and notebooks. Other then that...\n");
+                   System.out.println("Theres nothing in here....\n");                    
+                   result = false;
                 }
                 else {
                     System.out.println("Please enter a proper choice...");
